39c6c2a0c9a500cc3fc91066101c5136fd30ff2a
deepakverma9140 <developerdeepakverma@gmail.com>
2020-11-27 15:10:45 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -169,7 +169,17 @@ console.log(
   between(10, 200)
 )
 
+function makeid() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
+  for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+console.log('order' + makeid());
 
 
 // module.exports = getPost
