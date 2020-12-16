04c6bb7ade140799926907bc551a99f1d6d8416a
deepakverma91123 <deepakverma91123@gmail.com>
2020-12-16 12:33:25 +0530

    some updates

--- src/app/gateentry/gate-entry/gate-entry.component.ts
+++ src/app/gateentry/gate-entry/gate-entry.component.ts
@@ -92,7 +92,7 @@ export class GateEntryComponent implements OnInit {
 
     f.resetForm();
     this.snackBar.open('saved', '', { duration: 3000 });
-    this.router.navigate(['/landing']);
+    // this.router.navigate(['/landing']);
 
   }
 
