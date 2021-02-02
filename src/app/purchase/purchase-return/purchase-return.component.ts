05da482563036dfc8ae17c79aca1e76437f07c7d
deepakverma91123 <deepakverma91123@gmail.com>
2021-02-02 12:53:02 +0530

    Merge pull request #5 from deepakverma91123/neeraj
    Neeraj

--- src/app/purchase/purchase-return/purchase-return.component.ts
+++ src/app/purchase/purchase-return/purchase-return.component.ts
@@ -6,7 +6,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
 import { PurchaseserviceService } from '../purchaseservice.service';
 import { HttpClient } from '@angular/common/http';
-
+import { zip } from "rxjs";
+import { GatentryServiceService } from 'src/app/gateentry/gatentry-service.service';
 @Component({
   selector: 'app-purchase-return',
   templateUrl: './purchase-return.component.html',
@@ -21,8 +22,16 @@ export class PurchaseReturnComponent implements OnInit {
   post: any;
   Unit: any = [];
   today = new Date();
+  gate: any = [];
+  total: number;
+  gateTogle: string;
+  indent: any = [];
+  FullArray: any = [];
+  singlegateentry: any = [];
+  purchaseOrders: any = [];
   selectedCar: number;
-
+  singlepurchaseorderdetails: any = [];
+  singlegateentrydatails: any = [];
   cars = [
     { id: 1, name: 'Volvo' },
     { id: 2, name: 'Saab' },
@@ -30,8 +39,11 @@ export class PurchaseReturnComponent implements OnInit {
     { id: 4, name: 'Audi' },
   ];
   _id: string;
+  possible: string;
+  random: string;
   model: any = {};
-  constructor(public location: Location, private httpClient: HttpClient, private apiservice: ApiService, private purchaseservice: PurchaseserviceService, public snackBar: MatSnackBar,
+  updateId: string;
+  constructor(public location: Location, private httpClient: HttpClient, private apiservice: ApiService, private purchaseservice: PurchaseserviceService, private gateservice: GatentryServiceService, public snackBar: MatSnackBar,
     private router: Router, private route: ActivatedRoute, private _location: Location) {
     this._id = this.route.snapshot.paramMap.get('id');
 
@@ -100,6 +112,32 @@ export class PurchaseReturnComponent implements OnInit {
     this.router.navigate(['/landing']);
 
   }
+
+  myFun(purchaseorderid: string) {
+    // this.showForm = !this.showForm;
+    // this.updateId = purchaseorderid
+    zip(this.purchaseservice.getsinglepurchaseor(purchaseorderid), this.gateservice.getsinglegateentry(purchaseorderid))
+      .subscribe(([response1, response2]) => {
+
+        this.singlepurchaseorderdetails = response1;
+        console.log(this.singlepurchaseorderdetails)
+        this.purchaseOrders = this.singlepurchaseorderdetails.indetData.Tickets
+        this.total = this.purchaseOrders.reduce((a, b) => a + +b.reqQtys, 0)
+        console.log(this.singlepurchaseorderdetails.indetData.Tickets)
+
+        this.singlegateentry = response2;
+        // this.FullArray = this.singlegateentry.gateData
+        // this.FullArray = response2
+
+        this.FullArray = this.singlegateentry.gateData
+
+        console.log(this.FullArray)
+
+        // console.log(this.FullArray.truckWeight)
+
+      })
+  }
+
 
 
 
